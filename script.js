4a0ce82d3ff03681606346320fdaf25345540249
Akash M <108210779+akashikur@users.noreply.github.com>
2023-06-26 19:33:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
+	 s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
 }
 module.exports = palindrome
